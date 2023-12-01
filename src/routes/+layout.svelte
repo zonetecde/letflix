cb848c32e724f7786270966513ebfc840148c143
zonetecde <zonetecde@gmail.com>
2023-12-01 09:50:44 +0100

    + commencement page principale

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,29 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="h-screen w-screen">
+	<nav
+		class="bg-[#512727] h-12 md:h-20 flex flex-row relative items-center px-5 border-b-2 border-red-700 outline-b"
+	>
+		<div
+			class="absolute left-0 right-0 px-10 py-1 h-full flex flex-row items-center justify-center gap-4 bg-gradient-to-r from-[#c3adc500] via-[#ae2833] to-[#c3adc500]"
+		>
+			<img
+				src="letflix-white.png"
+				alt="Logo du site"
+				class="h-full object-contain cursor-pointer"
+				on:click={() => window.location.reload()}
+			/>
+			<img
+				src="github.png"
+				alt="GitHub logo"
+				class="absolute opacity-60 right-2 w-8 md:w-24 cursor-pointer"
+				on:click={() => window.open('https://github.com/zonetecde/letflix')}
+			/>
+		</div>
+	</nav>
+
+	<div class="h-full -mt-12 pt-12 md:-mt-20 md:pt-20">
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="w-full h-full bg-black text-white p-4">
+	<section
+		class="flex flex-col border-2 border-gray-600 rounded-b-xl h-48 px-2 p-1 shadow-xl bg-slate-900 shadow-gray-800"
+	>
+		<p>Nos meilleurs films</p>
+	</section>
+</div>
