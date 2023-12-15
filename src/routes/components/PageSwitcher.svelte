7cc9fd9e8eeca70a5ef5c37f76b789265b66e872
Rayane Staszewski <zonetecde@gmail.com>
2023-12-15 19:21:50 +0100

    Update dependencies in package.json and package-lock.json

--- src/routes/components/PageSwitcher.svelte
+++ src/routes/components/PageSwitcher.svelte
@@ -23,7 +23,7 @@
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
 			viewBox="0 0 24 24"
-			stroke-width="1.5"
+			stroke-width="1.49"
 			stroke={page == 1 ? 'gray' : 'white'}
 			class="h-14"
 		>
